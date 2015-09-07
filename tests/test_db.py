c4854274d2e0c2b3f873a2910b9183d8869f89fd
Isman Firmansyah <isman.firmansyah@gmail.com>
2015-09-08 04:18:36 +0700

    remove unnecessary methods

--- tests/test_db.py
+++ tests/test_db.py
@@ -3,33 +3,10 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import pytest
-from tinydb.storages import MemoryStorage
 
-
-@pytest.fixture(scope="session")
-def db():
+def test_database_init():
     from timo.db import Database
-    return Database(storage=MemoryStorage)
+    from tinydb.storages import MemoryStorage
 
-
-@pytest.fixture()
-def table(db):
-    return db.table("testing")
-
-
-def test_database_init(db):
+    db = Database(storage=MemoryStorage)
     assert db._conn.__class__.__name__ == "TinyDB"
-
-
-def test_database_table(table):
-    assert table.__class__.__name__ == "Table"
-
-
-def test_database_transaction(db, table):
-    assert db.transaction(table).__class__.__name__ == "transaction"
-
-
-def test_database_where(db, table):
-    table.insert({"id": 1})
-    assert table.search(db.where("id") == 1)
--- timo/db.py
+++ timo/db.py
@@ -11,34 +11,9 @@ from tinyrecord import transaction
 class Database(object):
     def __init__(self, *args, **kwargs):
         self._conn = TinyDB(*args, **kwargs)
-
-    def transaction(self, table):
-        """A shortcut to ``tinyrecord.transaction.transaction`` class.
-
-        Typical usage::
-
-            from timo.db import Database
-
-            db = Database("/path/to/database.json")
-            table = db.table("users")
-            with db.transaction(table) as tr:
-                tr.insert({"name": "test"})
-
-        :param table: An instance of ``tinydb.database.Table`` object.
-        :returns: An instance of ``tinyrecord.transaction.transaction``
-                  object.
-        """
-        return transaction(table)
-
-    def table(self, name, smart_cache=False, **kwargs):
-        """A shortcut to ``tinydb.TinyDB.table`` method.
-        """
-        return self._conn.table(name, smart_cache, **kwargs)
-
-    def where(self, key):
-        """A shortcut to ``tinydb.where`` object.
-        """
-        return where(key)
+        self.table = self._conn.table
+        self.where = where
+        self.transaction = transaction
 
     def __repr__(self):
         return "<{}: storage={}>".format(
